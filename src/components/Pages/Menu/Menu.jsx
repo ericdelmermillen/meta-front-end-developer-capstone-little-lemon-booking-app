ce7632314de3f27270e7e0e6e8d3b094e6a0511a
ericdelmermillen <ericdelmermillen@gmail.com>
2024-01-04 15:19:41 -0500

    initial stylings for Menu page

--- src/components/Pages/Menu/Menu.jsx
+++ src/components/Pages/Menu/Menu.jsx
@@ -2,13 +2,36 @@ import "./Menu.scss";
 
 const Menu = () => {
   return (
-    <>
-      <div className="menu">
-        <div className="menu__content">
-          <h1 className="menu__h1">from Menu page</h1>
+      <>
+        <div className="menu">
+          <div className="menu__content">
+            <div className="hero">
+              <div className="hero__left">
+                <h1 className="hero__heading">Our Menu</h1>
+                <h2 className="hero__subheading">
+                Simple, Fresh & Infused with Natural Goodness
+                </h2>
+                <p className="hero__blurb">
+                Explore a menu that captivates with its simple yet profound flavors.
+                </p>
+              </div>
+              <div className="hero__right">
+                <div className="hero__img--menu"></div>
+                <div className="hero__overlay"></div>
+              </div>
+  
+            </div>
+  
+          </div> 
+
+          <div className="menu__cards">
+            <h2 className="menu__specials">
+              This Weeks Specials
+            </h2>
+
+          </div>
         </div>
-      </div>
-    </>
-  )};
+      </>
+    )};
 
 export default Menu;
