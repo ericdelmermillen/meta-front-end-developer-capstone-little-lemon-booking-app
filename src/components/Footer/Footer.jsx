c188261454400453e00cfd7fac065e36a835a5b4
Eric Delmer Millen <ericdelmermillen@Erics-MacBook-Air.local>
2024-01-21 16:59:09 -0500

    added width: 100

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,9 @@ const Footer = () => {
               alt="Footer Logo" />
           </div>
           <div className="footer__right">
-            <p className="footer__copyright">&copy; Little Lemon Restaurant {new Date().getFullYear()} </p>
+            <p className="footer__copyright">
+              &copy; Little Lemon Restaurant {new Date().getFullYear()} 
+            </p>
           </div>
         </div>
       </footer>
